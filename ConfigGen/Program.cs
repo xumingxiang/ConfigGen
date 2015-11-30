c21729e9990cae61ba9a22dfffb7146d0462ffe8
xumingxiang <xumingxiang@plu.cn>
2015-11-30 11:41:27 +0800

    update

--- ConfigGen/Program.cs
+++ ConfigGen/Program.cs
@@ -12,13 +12,9 @@ namespace ConfigGen
         {
             string evn = args[0].Trim('\"');
             string projFold = args[1].Trim('\"');
-
             var templatePath = Directory.GetFiles(projFold, "appsettings.config.tpl", SearchOption.AllDirectories)[0];
-
             string templete = File.ReadAllText(templatePath);
-
             var re = new Regex(@"(?<tag>{\$.*})");
-
             MatchCollection mcs = re.Matches(templete);
             Dictionary<string, string> tags = new Dictionary<string, string>();
             foreach (Match mc in mcs)
@@ -27,15 +23,12 @@ namespace ConfigGen
                 tag_key = tag_key.Substring(2, tag_key.Length - 3);
                 tags[tag_key] = string.Empty;
             }
-
             string profilePath = Directory.GetFiles(projFold, "ConfigProfile.xml", SearchOption.TopDirectoryOnly)[0];
             string profile = File.ReadAllText(profilePath);
-
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(profile);
             var profileNode = xmlDoc.SelectSingleNode("profile");
             var evnNode = profileNode.SelectSingleNode(evn);
-
             var keys = tags.Keys.ToArray();
             for (int i = 0; i < keys.Length; i++)
             {
@@ -46,12 +39,10 @@ namespace ConfigGen
                     tags[key] = tagNode.InnerText;
                 }
             }
-
             foreach (var tag in tags)
             {
                 templete = templete.Replace("{$" + tag.Key + "}", tag.Value);
             }
-
             using (FileStream fs = new FileStream(templatePath.Substring(0, templatePath.Length - 4), FileMode.Create))
             {
                 using (StreamWriter sw = new StreamWriter(fs))
